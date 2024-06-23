5c584a4b6f4bde7a800202974dcbb27482a57af1
Sahil Soni <140968802+sahilsoni20@users.noreply.github.com>
2024-06-23 09:40:28 +0530

    LoginPage.js

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const {setUserInfo} = useContext(UserContext);
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('https://quill-nest.vercel.app/login', {
+    const response = await fetch('https://quill-nest.vercel-server.app/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
