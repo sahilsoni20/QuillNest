85b715c81c684067d7b92f0d8a35705a253ee76a
Sahil Soni <140968802+sahilsoni20@users.noreply.github.com>
2024-06-23 09:39:57 +0530

    EditPost.js

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('https://quill-nest.vercel.app/'+id)
+    fetch('https://quill-nest.vercel-server.app/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://quill-nest.vercel.app/post', {
+    const response = await fetch('https://quill-nest.vercel-server.app/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
