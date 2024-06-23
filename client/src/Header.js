023769ed9cc3a5efcccecbcca9a065ac8f80ced3
Sahil Soni <140968802+sahilsoni20@users.noreply.github.com>
2024-06-23 10:19:17 +0530

    Add files via upload

--- client/src/Header.js
+++ client/src/Header.js
@@ -5,7 +5,7 @@ import {UserContext} from "./UserContext";
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
-    fetch('https://quill-nest.vercel-server.app/profile', {
+    fetch('https://quill-nest-server.vercel.app/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -15,7 +15,7 @@ export default function Header() {
   }, []);
 
   function logout() {
-    fetch('https://quill-nest.vercel-server.app/logout', {
+    fetch('https://quill-nest-server.vercel.app/logout', {
       credentials: 'include',
       method: 'POST',
     });
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const {setUserInfo} = useContext(UserContext);
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('https://quill-nest.vercel-server.app/login', {
+    const response = await fetch('https://quill-nest-server.vercel.app/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
