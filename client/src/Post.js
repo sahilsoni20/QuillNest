1e8ec88f5c0f035e36cf7c913ae32a2ea3aa17e3
Sahil Soni <140968802+sahilsoni20@users.noreply.github.com>
2024-06-23 09:38:13 +0530

    Post.js

--- client/src/Post.js
+++ client/src/Post.js
@@ -7,7 +7,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://quill-nest.vercel.app/'+cover} alt=""/>
+          <img src={'https://quill-nest.vercel-server.app/'+cover} alt=""/>
         </Link>
       </div>
       <div className="texts">
