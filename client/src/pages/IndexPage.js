10ff9b96dbe2cfb6c9496b6dbfd00f59be69bd3c
Sahil Soni <140968802+sahilsoni20@users.noreply.github.com>
2024-06-23 09:29:41 +0530

    IndexPage.js

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:4000/post').then(response => {
+    fetch('https://quill-nest.vercel.app/post').then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
